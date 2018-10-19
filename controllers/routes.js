50b16b133d32d14f9e9363b976366a664e3ecb8e
Arben <arbenf@hotmail.se>
2018-10-19 15:32:35 +0200

    Added Mongoose

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,3 +1,50 @@
+let mongoose = require('mongoose');
+
+//Connect to database
+mongoose.connect('mongodb://ReceptNaringData:data123@ds137003.mlab.com:37003/recept', { useNewUrlParser: true });
+
+
+//Create a schema
+let receptSchema = new mongoose.Schema({
+    name: String,
+    persons: Number,
+    instructions: [String],
+    ingredients: [
+      {
+        name: String,
+        units: Number,
+        measuringUnit : String,
+        unitEquivalentInGrams: Number
+      }
+    ],
+    urlToImg : String
+});
+
+let Recept = mongoose.model('Recept', receptSchema);
+//save to database
+/*let omelett = Recept({
+        name: "Omelett",
+        persons: 4,
+        instructions: [
+          "Knäck äggen",
+          "Krydda äggsmeten med salt m.m.",
+          "Vispa äggen",
+          "Stek smeten",
+          "Lägg på tallrik"
+        ],
+        ingredients: [
+          {
+            name: "ägg",
+            units: 6,
+            measuringUnit : "st",
+            unitEquivalentInGrams: 60
+          }
+        ],
+        urlToImg : "/www/assets/omelett.jpg"
+      }).save(function(err){
+          if(err) throw err;
+          console.log('recept saved');
+      });*/
 
 const fs = require('fs');
 
@@ -21,17 +68,35 @@ module.exports = function(app){
 
     //Routes for index page
     app.get('/recept', (req, res)=>{
-        let searchWord = req.query;
-        res.render('index', {
-            recept: recept,
-            searchWord: searchWord
-        });
+        if(req.query.search){
+            const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+            Recept.find({name:regex}, function(err, data){
+                if(err){
+                    throw err;
+                } 
+                else{
+                    res.render('index', {
+                        recept: data,
+                    });
+                }
+            }); 
+        }
+        else{
+             //find all in database
+            Recept.find({}, function(err, data){
+                if(err) throw err;
+                res.render('index', {
+                    recept: data,
+                });
+            }); 
+        }
     });
 
-    app.post('/recept', urlEncodedParser, (req, res)=>{
+
+    /*app.post('/recept', urlEncodedParser, (req, res)=>{
         recept.push(req.body);
         res.json(recept);
-    });
+    });*/
 
     //----------------------------------
 
@@ -46,4 +111,9 @@ module.exports = function(app){
         newRecipe.push(req.body);
         res.json(newRecipe);
     });
+
+    function escapeRegex(text) {
+        return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+    };
+
 };
